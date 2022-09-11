648d9077a7e765231c744764a999fd071f8c346a
tobias2910 <33369379+tobias2910@users.noreply.github.com>
2022-09-11 23:49:15 +0200

    Adjusted the tests

--- tests/utils/utils.py
+++ tests/utils/utils.py
@@ -8,7 +8,7 @@ from src.config.settings import settings
 async def get_auth_token_header(client: AsyncClient) -> Dict[str, str]:
     credentials = {"email": settings.ADMIN_USER, "password": settings.ADMIN_PW}
 
-    response = await client.post(f"{settings.API_PATH}/auth/", json=credentials)
+    response = await client.post(f"{settings.API_PATH}/auth/login", json=credentials)
     auth_tokens = response.json()
     access_token = auth_tokens["access_token"]["token"]
     header = {"Authorization": f"Bearer {access_token}"}
